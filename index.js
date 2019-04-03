3a0e1a75ca21808677be632f34dccd4f2bcb6259
Dustie <46972221+LegendNed@users.noreply.github.com>
2019-04-03 21:48:59 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -8,8 +8,44 @@ bot.on("ready", () =>{
 })
 
 bot.on("message", async (message) => {
-    if (message.content === "ping") {
-        return message.channel.send("pong")
+    if (message.author.bot) return;
+    let prefix = config.prefix
+    if (!message.content.startsWith(prefix)) return;
+
+    let args = message.content.slice(prefix.length).trim().split(/ +/)
+    let cmd = args.shift().toLowerCase()
+
+    if (cmd === "ping") {
+        let botping = new Date() - message.createdAt
+        let apiping = bot.ping
+
+        return message.channel.send(`Bot ping: ${botping}ms\nApi ping: ${apiping}ms`)
+    }
+    else if (cmd === "kick") {
+        if (!message.member.roles.some(r => ["Moderators"].includes(r.name))) return message.reply("You do not have the moderator role!")
+
+        let member = message.mentions.members.first() || message.guild.members.get(args[0])
+        if (!member) return message.reply("This is an invalid user!")
+        if (!member.kickable) return message.reply("I cant kick this member as it is higher than me!")
+        let reason = args.slice(1).join(" ")
+        
+        await member.kick(reason).catch(error => {
+            return message.reply("There was an error try agian!")
+        })
+        return message.channel.send(`${member.user.tag} was kicked by ${message.author.tag}! Reason: ${reason}`)
+    }
+    else if (cmd === "ban") {
+        if (!message.member.roles.some(r => ["Moderators"].includes(r.name))) return message.reply("You do not have the moderator role!")
+
+        let member = message.mentions.members.first() || message.guild.members.get(args[0])
+        if (!member) return message.reply("This is an invalid user!")
+        if (!member.bannable) return message.reply("I cant ban this member as it is higher than me!")
+        let reason = args.slice(1).join(" ")
+
+        await member.ban(reason).catch(error => {
+            return message.reply("There was an error try agian!")
+        })
+        return message.channel.send(`${member.user.tag} was banned by ${message.author.tag}! Reason: ${reason}`)
     }
 })
 
